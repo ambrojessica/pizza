a19acc16cd29e0ce4c13cfcbaa82ee00f0708614
ambrojessica <ambrojessica0@gmail.com>
2022-05-22 12:30:00 -0600

    slideshow works, need to fix home page still

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import './PrettyPizza.css';
-import { useHistory } from 'react-router-dom';
+import { PizzaImages } from './slideshow/PizzaImages';
+import SlideshowPizza from './slideshow/SlideshowPizza';
 
 //beginning of function 
 const Home = () => {
@@ -16,33 +17,19 @@ const Home = () => {
   return (
     <div className='Home'>
       <nav>
-        <h1>Fiesta Pizza</h1>
         <div className="nav-button">
-          <Link to="/">Home</Link>
+          <Link to="/"><h1>Fiesta Pizza</h1></Link>
           <Link to="/cart">Cart</Link>
         </div>
       </nav>
-      <div className='intro'>
-        <h2>Get a pizza delivered while coding</h2>
-        <button onClick={routeToForm} id='order-pizza'>Order Pizza!</button>
-      </div>
-
-      <div className="container-logo">
-        <div className='logo1'>
-          <h3>PizzaHut</h3>
-        </div>
-        <div className='logo2'>
-          <h3>Dominos</h3>
-        </div>
-        <div className='logo3'>
-          <h3>Little Caesars</h3>
-        </div>
+      <div>
+        <SlideshowPizza slides={PizzaImages} />
       </div>
     </div>
   );
-
-
-
 };
 
 export default Home;
+
+{/* <button onClick={routeToForm} id='order-pizza'>Order Pizza!</button> */ }
+
