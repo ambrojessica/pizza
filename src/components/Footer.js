c4f87c5cff9f2d9bad1580e413e127a915290e41
ambrojessica <ambrojessica0@gmail.com>
2022-05-24 08:50:41 -0600

    slight changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="footer-info">
       <nav>
-        <img src="https://images.unsplash.com/photo-1547197309-082bdc50ce8b?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2738" alt="8 reasons to love pizza" />
+        <img src="https://images.unsplash.com/photo-1574647267419-cd3adf200aed?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769" alt="8 reasons to love pizza" />
         <div className="nav-info">
           <h3>Contact Us</h3>
           <address>
