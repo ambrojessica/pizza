3a833352845c252716d5b9985053dd457c5b5a71
Jessica Ambrocio <ambrojessica0@gmail.com>
2022-05-25 22:03:15 -0600

    pizza menu is displaying, needs fixing

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,11 +1,37 @@
 import React from "react";
+import pizzaData from "./data/pizzaData";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Pizza = () => {
   return (
     <div>
-      <p>hello</p>
+      <Header />
+      <div className="pizzaInfo">
+        {
+          pizzaData.map((item) => (
+            <DisplayPizza key={item} item={item} />
+          ))
+        }
+      </div>
+      <Footer />
     </div>
   );
 };
 
 export default Pizza;
+
+const DisplayPizza = ({ item }) => {
+  const { name, image, description, price } = item;
+  return (
+    <div className="pizzaCard">
+      <div className="testing">
+        <img src={image} alt="images of pizza" />
+        <h3>{name}</h3>
+        <h5>{description}</h5>
+        <p>{price}</p>
+      </div>
+    </div>
+
+  );
+};
