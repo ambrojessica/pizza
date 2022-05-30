a611cd95c040ecf50e6df26b30e166309d2234d7
Jessica Ambrocio <ambrojessica0@gmail.com>
2022-05-29 19:41:50 -0600

    some changes in data, and styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="footer-info">
       <nav>
-        <img src="https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" />
+        <img src="https://images.unsplash.com/photo-1542866788-0868e62ae8e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" />
         <div className="nav-info">
           <h3>Contact Us</h3>
           <address>
@@ -14,8 +14,8 @@ const Footer = () => {
             US 90001
           </address>
           <a href="tel:123-456-8888">123-456-8888</a>
-          <a href="mailto:peter.pepperoni@fiestapizza.com"
-          >peter.pepperoni@fiestapizza.com</a>
+          <a href="mailto:peter.pepperoni@justpizza.com"
+          >peter.pepperoni@justpizza.com</a>
         </div>
       </nav>
     </div>
--- src/components/data/pizzaData.js
+++ src/components/data/pizzaData.js
@@ -1,17 +1,17 @@
 const pizzaData = [
   {
     id: 1,
-    image: 'https://images.unsplash.com/photo-1633040248073-9a31468f7e99?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770',
-    name: 'The Healthy Heart',
-    description: 'Thick Crust, Original Sauce, Cheese, Corn, Jalapenos, Pepperoni, Red Bell Peppers & Olives',
-    price: 19.99,
-  },
-  {
-    id: 2,
     image: 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670',
     name: 'Basil & Tomato',
     description: 'Thin crust, Oregano sauce, mozzarella, basil & cut up tomatoes',
     price: 14.99,
+  },
+  {
+    id: 2,
+    image: 'https://images.unsplash.com/photo-1633040248073-9a31468f7e99?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770',
+    name: 'The Healthy Heart',
+    description: 'Thick Crust, Original Sauce, Cheese, Corn, Jalapenos, Pepperoni, Red Bell Peppers & Olives',
+    price: 19.99,
   },
   {
     id: 3,
@@ -29,9 +29,9 @@ const pizzaData = [
   },
   {
     id: 5,
-    image: 'https://images.unsplash.com/photo-1620374643762-9c0e91a22b10?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770',
+    image: 'https://images.unsplash.com/photo-1620374645466-dc3ff1558148?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
     name: 'The Usual',
-    description: 'Thin Crust, Original Red Sauce, Cheese, Italian Sausage',
+    description: 'Thin Crust, Original Red Sauce, Cheese, Mini Green Bell Peppers, Italian Sausage, Meat',
     price: 14.99,
   },
   {
