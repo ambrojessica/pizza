7f8ac2bc74c81e98c548740e6149a739a268e975
Jessica Ambrocio <ambrojessica0@gmail.com>
2022-05-30 08:50:43 -0600

    fixed footer problem

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import './PrettyPizza.css';
 
 const Footer = () => {
   return (
-    <div className="footer-info">
+    <div className="footer-info" id="footer">
       <nav>
         <img src="https://images.unsplash.com/photo-1542866788-0868e62ae8e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" />
         <div className="nav-info">
