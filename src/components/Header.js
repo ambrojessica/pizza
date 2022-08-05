2466f7d3343cbf981a04450fe0bc83ff73e70aaf
Jessica Ambrocio <ambrojessica0@gmail.com>
2022-08-05 12:59:03 -0600

    checking github

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 import { NavLink } from 'react-router-dom';
 import './PrettyPizza.css';
 import { FaCartPlus } from 'react-icons/fa';
-import { connect } from "react-redux";
 
 const Header = (props) => {
   return (
@@ -10,7 +9,7 @@ const Header = (props) => {
       <nav>
         <div className="nav-button">
           <h3>(123)-456-8888</h3>
-          <NavLink to="/"><h1>Just Pizza</h1></NavLink>
+          <NavLink to="/"><h1>Little Mateo's</h1></NavLink>
           <NavLink to="/cart"><FaCartPlus className='shoppingCart' />
           </NavLink>
         </div>
