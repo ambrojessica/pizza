6cce735e2aabf507c50f3b7d6ebea6301d104412
Jessica Ambrocio <ambrojessica0@gmail.com>
2022-08-13 18:05:58 -0600

    header styling done, working on cart next

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import './PrettyPizza.css';
-import { FaPhoneAlt, FaCartPlus } from 'react-icons/fa';
 import Cart from "./Cart";
 import { AiOutlineClose } from 'react-icons/ai';
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
+import { BsCart4, BsPhone } from 'react-icons/bs';
+import { GiFullPizza } from 'react-icons/gi';
 
 const Header = () => {
   const { quantity } = useSelector((state) => state.cart);
@@ -11,21 +12,21 @@ const Header = () => {
   const showSidebar = () => setSidebar(!sidebar);
 
   return (
-    <div>
-      <div className="header">
-        <h3><FaPhoneAlt />+1 (123)-456-8888</h3>
-        <h3>Little Mateo's</h3>
+    <div className="header-section">
+      <h3><BsPhone />+1 (123)-456-8888</h3>
+      <div className="header-title">
+        <h3>Little Mateo's <GiFullPizza /></h3>
+        <nav>
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#menu">Menu</a>
+          <a href="#contact">Contact</a>
+        </nav>
       </div>
-      <nav>
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#menu">Menu</a>
-        <a href="#contact">Contact</a>
-      </nav>
       <div>
         <span className="menu-bars">
           {quantity !== 0 && <span>{quantity}</span>}
-          <FaCartPlus onClick={showSidebar} />
+          <BsCart4 onClick={showSidebar} />
         </span>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
           <ul className='nav-menu-items' onClick={showSidebar}>
